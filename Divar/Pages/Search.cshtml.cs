e542ba8473710886591af36c3e6ae7a0c948f25d
mobinabageri <157510240+mobinabageri@users.noreply.github.com>
2024-12-08 19:49:35 +0330

    search

--- Divar/Pages/Search.cshtml.cs
+++ Divar/Pages/Search.cshtml.cs
@@ -1,7 +1,9 @@
 using Divar.Db;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Xml.Serialization;
 
@@ -11,22 +13,26 @@ namespace Divar.Pages
     {
         private readonly DivarContext _context;
         public readonly List<Ad> adment;
-        
+        public List<Ad> res;
         public SearchModel(DivarContext context)
         {
 
             _context = context;
             adment = _context.Ads.ToList();
         }
-        public void OnGet()
+        public void OnGet(List<Ad> searchresult)
         {
-
         }
 
         public void  OnPost(string search)
         {
-            var x=  adment.Where(m => m.Name == search|| m.Discription == search).ToList();
+            string searchres = search.Trim();
+
+
+            List<Ad> searchresult = _context.Ads.Where(a => a.Name.Contains(searchres) || a.Discription.Contains(searchres)).ToList() ;
+            res = (List<Ad>)(ViewData["result"] = searchresult);
         }
+
 
 
 
